ee0e72eb66b2654593b2e3d936ed62782b69df33
Ivan Junior <contato.ivanfrancajr@gmail.com>
2023-08-20 21:57:19 -0300

    fix: corrigido bug no navber mobile

--- src/js/main.js
+++ src/js/main.js
@@ -2,12 +2,17 @@ const toggleButton = document.querySelector("#btn-mobile");
 const navbarLinks = document.querySelector("#menu-mobile");
 const navbar = document.querySelector("header");
 
-
-
-
 window.addEventListener("scroll", () => {
   if (window.scrollY > 0) {
-    navbar.classList.add("fixed", "bg-[#1a1a1a]", "shadow-md", "w-full", "transition","duration-100"  );
+    navbar.classList.add(
+      "fixed",
+      'container',
+      "bg-[#1a1a1a]",
+      "shadow-md",
+      "w-full",
+      "transition",
+      "duration-100"
+    );
   } else {
     navbar.classList.remove("fixed", "bg-[#1a1a1a]", "shadow-md");
   }
@@ -23,10 +28,10 @@ toggleButton.addEventListener("click", () => {
   }
 });
 
-const menuLinks = document.querySelectorAll('#menu-mobile a');
-menuLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        navbarLinks.classList.add('hidden');
-        toggleButton.textContent = '☰';
-    });
+const menuLinks = document.querySelectorAll("#menu-mobile a");
+menuLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navbarLinks.classList.add("hidden");
+    toggleButton.textContent = "☰";
+  });
 });
