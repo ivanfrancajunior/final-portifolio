a9efc949141554a91e7176fbf9ae82fb02632ee6
Ivan <contato.ivanfrancajr@gmail.com>
2024-01-24 17:18:10 -0300

    feat: inciada versão 2.1

--- src/components/Container.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Container = ({children}:{children: React.ReactNode}) => {
-  return (
-    <section className='flex flex-col  items-start justify-center w-full  p-8 '>{children}</section>
-  )
-}
--- src/components/Label.tsx
+++
@@ -1,4 +0,0 @@
-
-export const Label = ({ label, id }: { label: string; id?:string }) => {
-  return <h1 className="text-4xl font-bold my-10 mt-16" id={id}>{label}</h1>;
-};
